c917a10c904f79fd1a01f79b62327fe5c3ff256e
Mathieu7 <mathieu.monnie@campus-la-chataigneraie.org>
2024-06-21 13:58:52 +0200

    Exercice Express avec un utilisateur un id ect hashage de MDP

--- index.js
+++ index.js
@@ -1,12 +1,100 @@
-import express from "express"
+import express from "express";
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs';
 
-const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Hello World!' })
-})
+const prisma = new PrismaClient();
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.post('/register', async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({
+      error: 'Champs obligatoires manquants',
+      missingFields: {
+        name: !name ? 'Nom est requis' : undefined,
+        email: !email ? 'Un Email est requis' : undefined,
+        password: !password ? 'Un mots de passe est requis' : undefined
+      }
+    });
+  }
+
+  try {
+    const hasedPassword = await bcrypt.hash(password, await bcrypt.genSalt(10));
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hasedPassword
+      }
+    });
+
+
+    res.status(201).json({ message: 'Utilisateur enregistrer avec succes', user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erreur du serveur' });
+  }
+});
+app.get('/user', async (req, res) => {
+  const {id} = req.query;
+
+  if (!id) {
+    return res.status(400).json({ error: 'ID invalide' });
+  }
+
+  try{
+    const user = await prisma.user.findUnique({
+      where: {
+        id: String(id)
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        password: false,
+      }
+    });
+
+    if (!user) {
+      return res.status(404).json({ error: 'Utilisateur non trouvé' });
+    }
+
+    res.status(200).json({ user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erreur du serveur' });
+  }
+});
+
+
+app.delete('/user', async (req, res) => {
+  const {id} = req.query;
+
+  if (!id) {
+    return res.status(400).json({ error: 'ID invalide' });
+  }
+
+  try {
+    const user = await prisma.user.delete({
+      where: {
+        id: String(id)
+      }
+    });
+
+    res.status(200).json({ message: 'Utilisateur supprimé avec succès', user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erreur du serveur' });
+  }
+});
+
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Exemple d'application sur le port${port}`);
+});
